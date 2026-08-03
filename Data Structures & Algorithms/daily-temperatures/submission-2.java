754294af45d993ce23abe2f2248ea7a3bc1874cb
Divyansh Shahi <103553952+Shahi1810@users.noreply.github.com>
2026-08-03 22:04:44 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<int[]> st = new Stack<>();
+        int[] arr = new int[temperatures.length];
+
+        for(int i=0;i<temperatures.length;i++){
+            int t = temperatures[i];
+            while(!st.isEmpty() && t>st.peek()[0]){
+                int[] pair = st.pop();
+                arr[pair[1]] = i-pair[1];
+            }
+            st.push(new int[]{t,i});
+        }
+        return arr;
+    }
+}
